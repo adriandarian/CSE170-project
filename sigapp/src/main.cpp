fc9e5c5d5ca9ed909d8d71dc76c60e34f3f55481
Adrian Darian <adarian@ucmerced.edu>
2019-12-12 11:17:42 -0800

    initial project structure

--- sigapp/src/main.cpp
+++ sigapp/src/main.cpp
@@ -5,8 +5,9 @@
 
 int main ( int argc, char** argv )
 {
-	MyViewer* v = new MyViewer ( -1, -1, 800, 600, "Graphics Support Code - SIG Polygon Editor" );
-	//v->cmd ( WsViewer::VCmdAxis );
+	MyViewer* v = new MyViewer ( -1, -1, 640, 480, "My SIG Application" );
+	v->cmd ( WsViewer::VCmdAxis );
+
 	v->view_all ();
 	v->show ();
 
--- sigapp/src/my_viewer.h
+++ sigapp/src/my_viewer.h
@@ -3,23 +3,23 @@
 # include <sig/sn_poly_editor.h>
 # include <sig/sn_lines2.h>
 
-# include <sigogl/ui_check_button.h>
-# include <sigogl/ui_slider.h>
+# include <sigogl/ui_button.h>
 # include <sigogl/ws_viewer.h>
 
 // Viewer for this example:
 class MyViewer : public WsViewer
 {  protected :
-	enum MenuEv { EvExit, EvViewCurveA, EvViewCurveB, EvDeltaT };
-	UiCheckButton *_viewA, *_viewB;
-	UiSlider* _slider;
-	SnPolyEditor* _polyed;
-	SnLines2 *_curveA;
-
+	enum MenuEv { EvNormals, EvAnimate, EvExit };
+	UiCheckButton* _nbut;
+	bool _animating;
    public :
 	MyViewer ( int x, int y, int w, int h, const char* l );
 	void build_ui ();
-	void update_scene ();
+	void add_model ( SnShape* s, GsVec p );
+	void build_scene ();
+	void show_normals ( bool view );
+	void run_animation ();
+	virtual int handle_keyboard ( const GsEvent &e ) override;
 	virtual int uievent ( int e ) override;
 };
 
