c50724abfb24ba35509e19c062879a2837a77aa5
dawang3 <dawang3@gmail.com>
2019-12-19 02:30:46 -0800

    Update my_viewer.h

--- sigapp/src/my_viewer.h
+++ sigapp/src/my_viewer.h
@@ -29,55 +29,39 @@ protected:
 	};
 	UiCheckButton *_nbut;
 	bool _animating;
-
-	// Camera Global Variables
 	bool cameraMovement = true;
-	float cameraX = 0, cameraY = 0, cameraZ = 0, cameraAdjustment = 1000.0f;
-
-	// Bounding Box
-	GsBox house1, house2;
-
-	// Shadow Global Variables
-	GsMat shadowPoints = {
-		1, // visibility?
-		0, // rotate across z-axis
-		0, // shift across the x-axis
-		0, // Does nothing to the shadow
-
-		0, // shift across y-axis and rotate across y-axis
-		0, // rotate across y-axis
-		0, // ?
-		0, // Does nothing to the shadow
-
-		// Leave alone for the shadow to be mounted to the Objects
-		0,  // ?
-		-1, // stretches across the z-axis
-		1,  // ?
-		0,  // ?
-
-		// Do Not Touch - Messes with the whole shadow
-		0, // ?
-		0, // ?
-		0, // ?
-		1  // ?
-	};
-
-	// Object Global Variables
-	float xTwoStoryHouse = 0.0,
-		  yTwoStoryHouse = 0.0, zTwoStoryHouse = 0.0;
+	float xTwoStoryHouse = 0.0, yTwoStoryHouse = 0.0, zTwoStoryHouse = 0.0;
+	float xBody = 0.0, yBody = 0.0, zBody = 0.0;
+	float xRightArm = 0.0, yRightArm = 0.0, zRightArm = 0.0;
+	float xLeftArm = 0.0, yLeftArm = 0.0, zLeftArm = 0.0;
+	float xFan = 0.0, yFan = 0.0, zFan = 0.0;
 	std::vector<std::vector<float>> pos = {
 		{xTwoStoryHouse, yTwoStoryHouse, zTwoStoryHouse},
+		{xBody, yBody, zBody},
+		{xRightArm, yRightArm, zRightArm},
+		{xLeftArm, yLeftArm, zLeftArm},
+		{xFan, yFan, zFan}
 	};
 	SnGroup *g = new SnGroup;
 	// SnGroup *TwoStoryHouse = new SnGroup;
+	SnGroup *Body = new SnGroup;
+	SnGroup *LeftArm = new SnGroup;
+	SnGroup *RightArm = new SnGroup;
+	SnGroup *Fan = new SnGroup;
 	SnModel *model[5];
 	SnModel *scene;
 	SnTransform *t[5];
 	GsMat m[5];
 	GsMat mt[5];
+	GsBox b0, b1, b2, b3, b4;
+	std::vector<GsBox> b = { b0, b1, b2, b3, b4};
 	std::vector<std::string> pieces = {
 		"mushroom-house.obj",
 	};
+	// std::vector<SnGroup *> sep = {
+	// 	TwoStoryHouse,
+	// };
+	std::vector<int> numRotations = {6, 3, 6, 6, 6};
 
 public:
 	MyViewer(int x, int y, int w, int h, const char *l);
@@ -85,17 +69,13 @@ public:
 	void add_model(SnShape *s, GsVec p);
 	void build_scene();
 	void show_normals(bool view);
-	void move_camera_right();
-	void move_camera_left();
-	void move_camera_up();
-	void move_camera_down();
-	void camera_zoom_in();
-	void camera_zoom_out();
-	void move_sun_right();
-	void move_sun_left();
-	void move_sun_up();
-	void move_sun_down();
 	void run_animation();
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+	void jumpUp();
+	void jumpDown();
 	virtual int handle_keyboard(const GsEvent &e) override;
 	virtual int uievent(int e) override;
 };
